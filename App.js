7d4f02e42fdf016dcfa40b14d2ced4da4811b489
saifurrehmankakar930 <saifurrehamkaka930@gmail.com>
2025-05-28 00:14:56 -0700

    alle code initial

--- App.js
+++ App.js
@@ -1,26 +1,9 @@
 
-// console.log(10 == '10')
+// let num1 = 5
+// let num2 =10
 
-// console.log(10 === '10')
+// let sum = num1 + num2
 
-// console.log(10 < 12)
-
-// console.log(10 !== '10')
-
-// console.log(10 != '10')
-
-// console.log(10 >= 10);
-
-// console.log(5 >= 6);
-
-// console.log(10 !== true)
-
-// let a = 5
-// let b =10
-
-// let sum = a + b
-
-// console.log()
 
 console.log('cat' !== 'dog' && 5 > 3 || 12 / 4 === 3 && 9 < 4 || 8 === 8)
     // true && true || true && false || true;
@@ -109,26 +92,91 @@ console.log( true && 'hello' === 'hello' || 9 * 2 === 18 && 6 + 2 === 8 || false
 // true || true || false
 // answer: true 
 
-// 'apple' === 'apple' && 2 + 3 === 5 || false && true || 10 - 5 === 5 || 5 === '5'
-
-// 'apple' === 'apple' && 3 + 4 === 7 || 6 * 2 !== 12 && 10 - 5 === 5 || 'Java' !== 'java'
-// 15 * 2 !== 30 && 5 === 5 || 'cat' === 'dog' && 7 / 2 === 3.5 || 20 > 10
-// 8 * 3 === 24 && 100 / 10 === 10 || 'red' !== 'green' && 9 + 1 === 10 || 5 === 5
-// 10 > 8 && 7 * 2 === 14 || 3 - 1 === 2 && 'sun' === 'sun' || 'hello' !== 'HELLO'
-// 0 !== '0' && 9 + 5 === 14 || 3 * 3 === 9 && 'apple' === 'apple' || 12 / 3 === 4
-// 'hi' !== 'HI' && 7 + 2 === 9 || 10 / 2 === 5 && 'bar' !== 'bar' || 3 > 4
-// 100 / 5 === 20 && 'dog' !== 'dog' || 12 * 2 === 24 && 9 === 9 || 'red' === 'blue'
-// 'true' !== true && 4 + 6 === 10 || 8 / 4 === 2 && 15 - 5 === 10 || 'yes' === 'YES'
-// 5 * 5 === 25 && 3 - 1 === 2 || 20 !== '20' && 10 === 10 || 'test' !== 'TEST'
-// 'cat' !== 'dog' && 4 * 5 === 20 || 7 + 2 === 9 && 2 !== 1 || 12 / 3 === 4
-// 9 > 8 && 6 * 2 === 12 || 3 + 7 !== 10 && 'apple' === 'apple' || 6 === '6'
-// 'hello' === 'hello' && 4 * 3 === 12 || 10 / 5 === 2 && 5 !== 5 || 'world' !== 'WORLD'
-// 20 - 5 === 15 && 'a' !== 'A' || 3 * 2 === 6 && 5 === '5' || 7 > 6
-// 4 + 4 === 8 && 6 * 3 === 18 || 5 !== '5' && 'dog' === 'dog' || 12 === 12
-// 'false' === 'false' && 10 - 4 === 6 || 3 / 3 === 1 && 7 === 7 || 8 * 2 === 16
-// 'blue' !== 'green' && 9 + 6 === 15 || 5 === 5 && 8 / 4 === 2 || 'apple' === 'banana'
-// 8 * 2 === 16 && 5 + 4 === 9 || 6 === '6' && 10 - 3 === 7 || 7 * 2 !== 16
-// 5 + 2 === 7 && 'red' === 'red' || 3 * 4 === 12 && 'dog' !== 'cat' || 10 - 5 === 5
-// 7 * 3 === 21 && 12 / 4 === 3 || 5 + 6 === 11 && 'blue' !== 'green' || 'yes' !== 'YES'
-// 'false' === false && 4 * 2 === 8 || 3 - 2 === 1 && 10 / 5 === 2 || 'a' !== 'A'
-// 6 + 6 === 12 && 'yellow' === 'yellow' || 8 !== 8 && 3 * 3 === 9 || 'hello' !== 'HELLO'
+console.log ('apple' === 'apple' && 2 + 3 === 5 || false && true || 10 - 5 === 5 || 5 === '5')
+  // true && true || false && true || true && false
+  // true || false || false
+  // answer: true 
+console.log ('apple' === 'apple' && 3 + 4 === 7 || 6 * 2 !== 12 && 10 - 5 === 5 || 'Java' !== 'java')
+  // true && true || false && true || true 
+  // true || false || true
+  // answer: true 
+console.log  (15 * 2 !== 30 && 5 === 5 || 'cat' === 'dog' && 7 / 2 === 3.5 || 20 > 10)
+   // false && true || false && true || true 
+   // false || false || true
+   // answer: true
+console.log(8 * 3 === 24 && 100 / 10 === 10 || 'red' !== 'green' && 9 + 1 === 10 || 5 === 5)
+   // true && true || true && true || true 
+   // true ||true || true
+   // answer: true
+console.log( 10 > 8 && 7 * 2 === 14 || 3 - 1 === 2 && 'sun' === 'sun' || 'hello' !== 'HELLO')
+   // true && true || true && true || true 
+   // true ||true || true
+   // answer: true
+console.log(0 !== '0' && 9 + 5 === 14 || 3 * 3 === 9 && 'apple' === 'apple' || 12 / 3 === 4)
+   // false && true || true && true || true 
+   // false ||true || true
+   // answer: true
+console.log('hi' !== 'HI' && 7 + 2 === 9 || 10 / 2 === 5 && 'bar' !== 'bar' || 3 > 4)
+   // true && true || true && false || false 
+   // true ||false || false
+   // answer: true
+console.log(100 / 5 === 20 && 'dog' !== 'dog' || 12 * 2 === 24 && 9 === 9 || 'red' === 'blue')
+   // true && false || true && true || false 
+   // false ||true || false
+   // answer: true
+console.log('true' !== true && 4 + 6 === 10 || 8 / 4 === 2 && 15 - 5 === 10 || 'yes' === 'YES')
+   // false && true || true && true || false 
+   // false ||true || false
+   // answer: true
+console.log( 5 * 5 === 25 && 3 - 1 === 2 || 20 !== '20' && 10 === 10 || 'test' !== 'TEST')
+   // true && true || false && true || true 
+   // true || false || true
+   // answer: true
+console.log( 'cat' !== 'dog' && 4 * 5 === 20 || 7 + 2 === 9 && 2 !== 1 || 12 / 3 === 4)
+   // true && true || true && false || true 
+   // true || false || true
+   // answer: true
+console.log (9 > 8 && 6 * 2 === 12 || 3 + 7 !== 10 && 'apple' === 'apple' || 6 === '6')
+   // true && true || true && true || false 
+   // true || true || false
+   // answer: true
+console.log('hello' === 'hello' && 4 * 3 === 12 || 10 / 5 === 2 && 5 !== 5 || 'world' !== 'WORLD')
+   // true && true || true && false || true 
+   // true || false || true
+   // answer: true
+console.log(20 - 5 === 15 && 'a' !== 'A' || 3 * 2 === 6 && 5 === '5' || 7 > 6)
+   // true && true || true && true || true 
+   // true || true || true
+   // answer: true
+console.log( 4 + 4 === 8 && 6 * 3 === 18 || 5 !== '5' && 'dog' === 'dog' || 12 === 12)
+   // true && true || true && true || true 
+   // true || true || true
+   // answer: true
+console.log( 'false' === 'false' && 10 - 4 === 6 || 3 / 3 === 1 && 7 === 7 || 8 * 2 === 16)
+   // true && true || true && true || true 
+   // true || true || true
+   // answer: true
+console.log( 'blue' !== 'green' && 9 + 6 === 15 || 5 === 5 && 8 / 4 === 2 || 'apple' === 'banana')
+   // true && true || true && true || false
+   // true || true || false
+   // answer: true
+console.log(8 * 2 === 16 && 5 + 4 === 9 || 6 === '6' && 10 - 3 === 7 || 7 * 2 !== 16)
+    // true && true || false && true || true 
+    // true || false || true
+    // answer: true
+console.log(5 + 2 === 7 && 'red' === 'red' || 3 * 4 === 12 && 'dog' !== 'cat' || 10 - 5 === 5)
+    // true && true || true && true || true 
+    // true || true || true
+    // answer: true
+console.log(7 * 3 === 21 && 12 / 4 === 3 || 5 + 6 === 11 && 'blue' !== 'green' || 'yes' !== 'YES')
+   // true && true || true && true || true 
+   // true || true || true
+   // answer: true
+console.log('false' === false && 4 * 2 === 8 || 3 - 2 === 1 && 10 / 5 === 2 || 'a' !== 'A')
+   // false && true || true && true || true 
+   // fals || true || true
+   // answer: true
+console.log(6 + 6 === 12 && 'yellow' === 'yellow' || 8 !== 8 && 3 * 3 === 9 || 'hello' !== 'HELLO')
+   // true && true || false && true || true 
+   // true || false || true
+   // answer: true
